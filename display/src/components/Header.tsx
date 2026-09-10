72bea9485a4a2255eb2d22608adbc70946907a8d
vs-tries-to-code <vishnusree1805@gmail.com>
2026-09-11 00:43:05 +0530

    Modified size of drishti logo of header

--- display/src/components/Header.tsx
+++ display/src/components/Header.tsx
@@ -2,14 +2,14 @@ import { Link } from "react-router-dom"
 
 export default function Header() {
   return (
-    <header className="flex justify-between items-start">
-        <div className="w-[20%]">
-            <Link to="/" className="flex p-5 items-center gap-3">
-                <img src="/neuracet-white.svg" alt="NeuraCET" className="h-10" />
-                <p className="text-[2.5rem]  font-extralight text-white">NeuraCET</p>
+    <header className="flex justify-between items-center h-20 px-6 bg-black">
+        <div className="flex-1">
+            <Link to="/" className="flex items-center gap-3">
+                <img src="/neuracet-white.svg" alt="NeuraCET" className="h-12" />
+                <p className="text-2xl font-extralight text-white">NeuraCET</p>
             </Link>
         </div>
-        <img src="/drishti-logo.png" alt="Drishti" className="h-35 p-5 "/>
+        <img src="/drishti-logo.png" alt="Drishti" className="h-12" />
     </header>
   )
 }
